b3bd1cd2f3e8c088504c19aef9b9fe0d1137809a
rahulp1262003 <prajapatirahul1262003@gmail.com>
2024-04-20 18:40:08 +0530

    update

--- upload.php
+++ upload.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Compressor</title>
     <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -71,7 +74,7 @@ if (isset($_POST['submit'])) {
         imagedestroy($original_image);
         imagedestroy($compressed_image);
     } else {
-        echo "Error uploading file.";
+        echo "<div class='errormassege'> Error uploading file.</div> ";
     }
 }
 
